b98745e7e4c6b6f53becdae358c58406d918a548
NightWave2 <163851344+NightWave2@users.noreply.github.com>
2024-03-18 23:18:30 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import grader
+
+st.header("Basic model")
+question = st.text_input("Enter the question:")
+ref_ans = st.text_area("Enter the reference answer:")
+answer1 = st.text_area("Enter the answer 1:")
+answer2 = st.text_area("Enter the answer 2:")
+answer3 = st.text_area("Enter the answer 3:")
+
+if st.button("Compute"):
+    answers = [answer1,answer2,answer3]
+    grades = grader.grader(question,ref_ans,answers)
+    st.divider()
+    st.write("Similarity for _Answer_ 1: ",grades[0],size=30)
+    st.write("Similarity for _Answer_ 2: ",grades[1],size=30)
+    st.write("Similarity for _Answer_ 3: ",grades[2],size=30)
+
+
+
