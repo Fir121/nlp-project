748558bdd50fa17366003d47702e41f349a5e35b
Mohamed Firas <62911996+Fir121@users.noreply.github.com>
2024-04-29 18:13:30 +0400

    pages kinda here

--- server/main.py
+++ server/main.py
@@ -1,19 +1,6 @@
 from flask import Flask, request, jsonify, render_template, abort
-import bcrypt
-import uuid
 
 app = Flask(__name__)
-
-# Sample user database
-users = [{
-        'id': str(uuid.uuid4()),
-        'name': 'Test User',
-        'email': 'test@example.com',
-        'password': '$2b$12$vTTgl50tjnztWcPqQzt5Ve4TGsnYYII/wNrC2y2Iu2E9WZj.Hwuk2'
-    }]
-
-def hash_password(password):
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 @app.route('/')
 def index():
@@ -32,8 +19,8 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     
-    email = request.json.get('email')
-    password = request.json.get('password')
+    email = request.form.get('email')
+    password = request.form.get('password')
 
     abort(500)
 
@@ -41,13 +28,25 @@ def login():
 def signup():
     if request.method == 'GET':
         return render_template('signup.html')
-    name = request.json.get('name')
-    email = request.json.get('email')
-    password = request.json.get('password')
-    repassword = request.json.get('repassword')
-    teacher_or_student = request.json.get('selectedtoggle')
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    repassword = request.form.get('repassword')
+    teacher_or_student = request.form.get('selectedtoggle')
 
     abort(500)
+
+@app.route("/teacher/dashboard")
+def teacher_home():
+    return render_template('teacher/index.html')
+
+@app.route("/student/dashboard")
+def student_home():
+    return render_template('student/index.html')
+
+@app.route("/student/page2")
+def student_page2():
+    return render_template('student/page2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
